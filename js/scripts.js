d345651c92275b422c43559c9eccb12e323a58df
toddmac12 <toddmac12@gmail.com>
2021-08-15 16:45:57 -0400

    added more survey questions and styling effects to result cardsupdate to styles.css file

--- js/scripts.js
+++ js/scripts.js
@@ -5,8 +5,6 @@ $(document).ready(function() {
 
     $("#formRadio1").submit(function(event) {
         event.preventDefault();
-
-
         const bestProgramAnswer1 = $("input:radio[name=answer1]:checked").val();
         if (bestProgramAnswer1 === "JavaScript") {
             $("#formRadio1").hide();
@@ -41,39 +39,65 @@ $(document).ready(function() {
             return Python++;
         } else {}
     });
-
-
     $("#formRadio3").submit(function(event) {
         event.preventDefault();
         const bestProgramAnswer3 = $("input:radio[name=answer3]:checked").val();
         if (bestProgramAnswer3 === "JavaScript") {
-            JavaScript++;
             $("#formRadio3").hide();
-
-
+            $("#formRadio4").show();
+            return JavaScript++;
         } else if (bestProgramAnswer3 === "Ruby") {
-            Ruby++;
             $("#formRadio3").hide();
-
+            $("#formRadio4").show();
+            return Ruby++;
         } else if (bestProgramAnswer3 === "Python") {
-            Python++;
             $("#formRadio3").hide();
-
+            $("#formRadio4").show();
+            return Python++;
         } else {}
+    });
+    $("#formRadio4").submit(function(event) {
+        event.preventDefault();
+        const bestProgramAnswer4 = $("input:radio[name=answer4]:checked").val();
+        if (bestProgramAnswer4 === "JavaScript") {
+            $("#formRadio4").hide();
+            $("#formRadio5").show();
+            return JavaScript++;
+        } else if (bestProgramAnswer4 === "Ruby") {
+            $("#formRadio4").hide();
+            $("#formRadio5").show();
+            return Ruby++;
+        } else if (bestProgramAnswer4 === "Python") {
+            $("#formRadio4").hide();
+            $("#formRadio5").show();
+            return Python++;
+        } else {}
+    });
 
+
+    $("#formRadio5").submit(function(event) {
+        event.preventDefault();
+        const bestProgramAnswer5 = $("input:radio[name=answer5]:checked").val();
+        if (bestProgramAnswer5 === "JavaScript") {
+            JavaScript++;
+            $("#formRadio5").hide();
+        } else if (bestProgramAnswer5 === "Ruby") {
+            Ruby++;
+            $("#formRadio5").hide();
+        } else if (bestProgramAnswer5 === "Python") {
+            Python++;
+            $("#formRadio5").hide();
+        } else {}
         $("#showResult").show();
         if (JavaScript >= 2) {
-
             $("#JavaScriptResult").show();
         } else if (Ruby >= 2) {
-
             $("#RubyResult").show();
         } else if (Python >= 2) {
             $("#PythonResult").show();
         } else {
             $("#noresult").show()
         }
-
         $("#btnReset").click(function() {
             location.reload();
         });
